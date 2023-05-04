c60188249918c16b4f7040d4a52a597cf57c83e6
Abram <58718191+4b3c@users.noreply.github.com>
2023-05-03 21:33:08 -0700

    variables

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+using namespace std;
+
 int main() {
-	std::cout << "Hello World!";
+	int a = 1;
+	int b = 2;
+
+	int c = a;
+	a = b;
+	b = c;
+	
+	cout << b;
+
 	return 0;
 }
