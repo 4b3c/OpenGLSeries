d9f65ecffc5aab2d2e86bcae439932271f0c786d
Abram <58718191+4b3c@users.noreply.github.com>
2023-05-03 22:04:31 -0700

    testing input

--- main.cpp
+++ main.cpp
@@ -3,12 +3,10 @@
 using namespace std;
 
 int main() {
-	double sales = 95000;
-	double state_tax = 0.04 * sales;
-	double county_tax = 0.02 * sales;
-	cout << "Sales: " << sales << endl;
-	cout << "State Tax: " << state_tax << endl;
-	cout << "County Tax: " << county_tax << endl;
-	cout << "Total Profit: " << sales - state_tax - county_tax << endl;
+	string name;
+	cout << "What's your name? ";
+	cin >> name;
+	cout << "Hello " << name;
+	
 	return 0;
 }
