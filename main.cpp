a425f3e69e066aa5cfaa7540b4428f6cabd29ff8
Abram <58718191+4b3c@users.noreply.github.com>
2023-05-03 21:55:45 -0700

    sales exercize

--- main.cpp
+++ main.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-
+#include <sstream>
 using namespace std;
 
 int main() {
-	int a = 1;
-	int b = 2;
-
-	int c = a;
-	a = b;
-	b = c;
-	
-	cout << b;
-
+	double sales = 95000;
+	double state_tax = 0.04 * sales;
+	double county_tax = 0.02 * sales;
+	cout << "Sales: " << sales << endl;
+	cout << "State Tax: " << state_tax << endl;
+	cout << "County Tax: " << county_tax << endl;
+	cout << "Total Profit: " << sales - state_tax - county_tax << endl;
 	return 0;
 }
