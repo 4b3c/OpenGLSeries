b46de19f7fd1f3e00bc66d000e7b8d2b4da68caf
Abram <58718191+4b3c@users.noreply.github.com>
2023-05-04 18:59:19 -0700

    made a number guesser

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
-#include <cmath>
 #include <cstdlib>
 #include <ctime>
 using namespace std;
 
 int main() {
-	int number;
-	cout << "Give me a number: ";
-	cin >> number;
-	if ((number % 2) == 0) {
-		cout << "Even!";
-	} else {
-		cout << "Odd!";
+	srand(time(0));
+	int num = rand() % 100;
+	int guessed = 101;
+	while (num != guessed) {
+		cout << "Guess the random number: ";
+		cin >> guessed;
+
+		if (num > guessed) {
+			cout << "You guessed too low..." << endl;
+		} else if (num < guessed) {
+			cout << "You guessed too high..." << endl;
+		}
+
 	}
+	cout << "Congratulations, you got it. It was " << num << ".\n";
 	return 0;
 }
