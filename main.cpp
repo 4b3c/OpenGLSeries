7d52b39dac3cc9a4f61c875d9cd5e3f6c5806334
Abram <58718191+4b3c@users.noreply.github.com>
2023-05-04 22:17:41 -0700

    word frequency counter (chatgpt's challenge)

--- main.cpp
+++ main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 #include <fstream>
+#include <unordered_map>
 using namespace std;
 
-int binary_search(int arr[], int target, int low, int high) {
-	int middle = (high + low) / 2; 
-	if (arr[middle] == target) {
-		return middle;
-	} else if (arr[middle] > target) {
-		return binary_search(arr, target, low, middle);
-	} else {
-		return binary_search(arr, target, middle, high);
-	}
-}
-
 int main() {
-	int target = 6;
-	int array[] = {1, 4, 6, 7, 8, 10, 13, 15, 16, 17, 20, 22, 25, 26, 29, 32, 33, 34, 35, 53, 64};
+	unordered_map<string, int> word_freq;
 
-	cout << binary_search(array, target, 0, (sizeof(array) / sizeof(array[0]))) << endl;
+	fstream file;
+	string str_line;
+	int lasti = 0;
+
+	file.open("words.txt");
+	getline(file, str_line);
+	file.close();
+
+	for (int i = 0; i < str_line.length(); i++) {
+		if (str_line[i] == ' ' || str_line[i] == '.' || str_line[i] == ',') {
+			word_freq[str_line.substr(lasti, i - lasti)] += 1;
+			lasti = i + 1;
+		}
+	}
+
+	for (auto const& [key, value] : word_freq) {
+		cout << key << ": " << value << endl;
+	}
 
 	return 0;
 }
