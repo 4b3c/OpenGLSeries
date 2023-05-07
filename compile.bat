10766e68c35868e6afc4f1bac5aca3bab36a1fec
Abram <58718191+4b3c@users.noreply.github.com>
2023-05-06 17:10:53 -0700

    made it into an empty visual studio project

--- compile.bat
+++
@@ -1,5 +0,0 @@
-set /p FILENAME= "Filename: "
-echo Compiling and running %FILENAME%
-cd "C:\Users\Abram P\Desktop\Programming\C++\IsometricRevisit"
-g++ "%FILENAME%.cpp" -o %FILENAME%
-start cmd.exe /k "%CD%\%FILENAME%"
